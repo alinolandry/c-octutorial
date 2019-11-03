caac780aeb4f1b4afda788616aabdabd8fadcd19
Alain Noutchomwo <alinolandry@gmail.com>
2019-11-03 11:08:18 -0500

    Project initialisation and end of TP1 OC tutorial

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tp1 obj = new Tp1();
+            obj.Run();
+           
+            
+            /*
+             *  Introduction in C# language
+             * 
+            ShowWelcome();
+            Introduction();
+            SayHello("Pulcherie", 23);
+
+            //double sol = CalHypotenuse(5,3);
+            Console.WriteLine("Le resultat est "+ CalHypotenuse(5, 3));
+            Console.WriteLine("Utilisateur actuel: " + Environment.UserName + " Connecté le " + DateTime.Now);
+            */
+        }
+
+        // Simple method to say hello
+        static void SayHello(string name, int age)
+        {
+            Console.WriteLine("Bonjour " + name);
+            Console.WriteLine("Vous avez " + age + " ans");
+        }
+
+        // Metod calcul something
+        static double CalHypotenuse(double a, double b)
+        {
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("\n Calcul la la longueur de l'hypothenuse d'un triangle de longueur " +a+ "et largeur "+b);
+            double kSum = a * a + b * b;
+            double resultat = Math.Sqrt(kSum);
+            return resultat;
+        }
+
+
+        // simple method for introduction in C# language
+        static void Introduction()
+        {
+            int val = 5;
+            string nom = "Alain Nkouamen";
+            if (val > 0)
+            {
+                Console.WriteLine("valeur superieur a zero " + nom);
+            }
+            else
+            {
+                Console.WriteLine("valeur inférieure à zero " + nom);
+            }
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("je suis dans le C#");
+        }
+
+        // Anothere methode to show Welcome message
+        static void ShowWelcome()
+        {
+            Console.WriteLine("Good Morning Sir");
+            Console.WriteLine("-----------------" + Environment.NewLine);
+            Console.WriteLine("\t Welcome in the C# World");
+        }
+    }
+}
---
+++ Tp1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Tp1
+    {
+        public void Run()
+        {
+            ShowWelcomeMsg();
+            Logic();
+        }
+
+        public void ShowWelcomeMsg()
+        {
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("--------------------------------------  WELCOME TO MY FIRST TP C# APP --------------------------------------------------");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+        }
+
+        public bool TestHour(int hour)
+        {
+            /*
+             * This method  return true if hour between 9a.m - 6p.m and false if between 6p.m and 9a.m
+             * 
+             */
+            bool result;
+            if(hour>=9 && hour < 18)
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        public bool TestAllWeek(string today)
+        {
+            /*
+             * This method  return true if the day of today is between L, M, M,J and V
+             * 
+             */
+            switch (today)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                    return true;
+            }
+            return false;
+        }
+
+        public bool TestContractWeek(string today)
+        {
+            /*
+             * This method  return true if the day of today is between L, M, M,J
+             * 
+             */
+            switch (today)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                    return true;
+            }
+            return false;
+        }
+
+        public void Logic()
+        {
+           // string username = Environment.UserName;
+           // int hour = DateTime.Now.Hour;
+           // string today = DateTime.Now.DayOfWeek.ToString();
+           // string msg = "";
+            string username = "Sorel";
+            int hour = 12;
+            string today = "Friday";
+            string msg = "";
+
+            if (TestHour(hour))
+            {
+                if (TestAllWeek(today))
+                {
+                    msg = "-----     Bonjour "+ username +"        ------";
+                }
+                else
+                {
+                    msg = "-----     Bon Weekend " + username + "   ------";
+                }
+            }
+            else
+            {
+                if (TestContractWeek(today))
+                {
+                    msg = "-----     Bonsoir " + username + "        ------";
+                }
+                else
+                {
+                    msg = "-----     Bon Weekend " + username + "   ------";
+                }
+            }
+            Console.WriteLine(msg);
+        }
+    }
+}
